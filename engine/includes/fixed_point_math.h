aeb33083da6ca4964a8a06984101159d5ab13a52
TebaHa <Tchurlyaev.teba@yandex.com>
2019-07-05 21:12:36 +0300

    initial commit

---
+++ engine/includes/fixed_point_math.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed_point_math.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zytrams <zytrams@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/05 19:56:21 by zytrams           #+#    #+#             */
+/*   Updated: 2019/07/05 21:09:38 by zytrams          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_POINT_MATH_H
+
+void	clearbufbyte(void *dst, int c, int a)
+{
+
+}
+
+void	copybuf(void *src, void *dst, int c)
+{
+
+}
+
+#endif
