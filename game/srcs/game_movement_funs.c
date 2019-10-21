db8989fea49f2b79abaa0ba5d65a7b09ec71e93b
TebaHa <Tchurlyaev.teba@yandex.com>
2019-10-21 15:08:46 +0300

    absabs

--- game/srcs/game_movement_funs.c
+++ game/srcs/game_movement_funs.c
@@ -6,7 +6,7 @@
 /*   By: zytrams <zytrams@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 19:17:27 by zytrams           #+#    #+#             */
-/*   Updated: 2019/10/21 15:05:54 by zytrams          ###   ########.fr       */
+/*   Updated: 2019/10/21 15:08:35 by zytrams          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ void	move_player(t_engine *eng, t_player *plr, t_point_2d dd, unsigned sect)
 void	change_floor(t_engine *eng, int sect, int change)
 {
 	if (abs(eng->world->sectors_array[sect].floor) < 8000
-	&& abs(eng->world->sectors_array[sect].ceil
-	- eng->world->sectors_array[sect].floor + change) > 200)
+	&& abs(eng->world->sectors_array[sect].floor
+	- eng->world->sectors_array[sect].ceil + change) > 200)
 		eng->world->sectors_array[sect].floor += change;
 }
 
 void	change_ceil(t_engine *eng, int sect, int change)
 {
 	if (abs(eng->world->sectors_array[sect].ceil) < 8000
-	&& abs(eng->world->sectors_array[sect].floor
-	- eng->world->sectors_array[sect].ceil + change) > 200)
+	&& abs(eng->world->sectors_array[sect].ceil
+	- eng->world->sectors_array[sect].floor + change) > 200)
 		eng->world->sectors_array[sect].ceil += change;
 }
 
